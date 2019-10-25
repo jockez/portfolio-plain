0bb1379ec87d47f98fd10bdef4a76250317c518a
jockez <lollive@live.se>
2019-10-25 21:06:37 +0200

    failsafe if modal is not found

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,26 +1,28 @@
-btns = document.getElementsByClassName('btn');
-closeBtns = document.getElementsByClassName('close-btn');
-overlay = document.getElementById('modal-overlay');
+    btns = document.getElementsByClassName('btn');
+    closeBtns = document.getElementsByClassName('close-btn');
+    overlay = document.getElementById('modal-overlay');
 
 
-for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function () {
-        const project = btns[i].dataset.project;
-        const modal = document.getElementById(project + "-modal");
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+            const project = btns[i].dataset.project;
+            const modal = document.getElementById(project + "-modal");
+            // Only if modal found
+            if (modal) {
+                // Remove hidden class from modal and overlay
+                overlay.classList.remove("hidden");
+                modal.classList.remove("hidden");
+            }
+        });
+    }
 
-        // Remove hidden class from modal and overlay
-        overlay.classList.remove("hidden");
-        modal.classList.remove("hidden");
-    });
-}
+    for (let i = 0; i < closeBtns.length; i++) {
+        closeBtns[i].addEventListener("click", function () {
+            const modalId = overlay.childNodes[1].id;
+            modal = document.getElementById(modalId);
 
-for (let i = 0; i < closeBtns.length; i++) {
-    closeBtns[i].addEventListener("click", function () {
-        const modalId = overlay.childNodes[1].id;
-        modal = document.getElementById(modalId);
-
-        // Add hidden class to modal and overlay
-        modal.classList.add("hidden");
-        overlay.classList.add("hidden");
-    });
-}
+            // Add hidden class to modal and overlay
+            modal.classList.add("hidden");
+            overlay.classList.add("hidden");
+        });
+    }
